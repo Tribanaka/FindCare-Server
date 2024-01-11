6009f9a70dfc21c9fb5edb5264418410690d62d5
Daniel <danielukoha101@gmail.com>
2024-01-11 12:49:57 +0100

    feat: add auth gaurd

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { HospitalsController } from './hospitals/hospitals.controller';
 import { HospitalsService } from './hospitals/hospitals.service';
 import { PractionersController } from './practioners/practioners.controller';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot()],
   controllers: [AppController, HospitalsController, PractionersController],
   providers: [AppService, HospitalsService],
 })
