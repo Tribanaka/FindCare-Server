b99d171ec38d37b387eee642bde1076b1f9d1d77
Daniel <danielukoha101@gmail.com>
2024-01-14 23:57:20 +0100

    feat: nest schedule validation

--- src/schedules/dto/create-schedule.dto.ts
+++ src/schedules/dto/create-schedule.dto.ts
@@ -6,10 +6,13 @@ import {
   ValidateNested,
 } from 'class-validator';
 
+import { Type } from 'class-transformer';
+
 export class CreateSchedulesDto {
   @IsArray()
   @ArrayMinSize(1)
   @ValidateNested({ each: true })
+  @Type(() => Schedule)
   schedules: Schedule[];
 }
 
