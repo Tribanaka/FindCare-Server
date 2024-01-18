2595aa48bd8eb80ba6b4d4e6852e3e4cd5b3f94e
Daniel <danielukoha101@gmail.com>
2024-01-18 12:47:34 +0100

    fix: app not found on railway

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// Use `PORT` provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
